3281f5e144808a5f4eb472e4927581aedfee5c05
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-09 23:41:55 -0500

    Adding active record dummy

---
+++ spec/active_record_dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveRecordDummy::Application.config.secret_token = '7649b2ab2948698784969c0064ead4385e9114b186508272560448c8fefcb4ccb5dc6311bb5ff633eb5b835bda6bf79add993290dd447de3b9dfcfe0fbe5a416'
