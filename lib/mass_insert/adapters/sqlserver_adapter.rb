deb6d68cece096db27db450262f405ccbd174b13
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-13 23:43:31 -0500

    Including SQLServer adapter

---
+++ lib/mass_insert/adapters/sqlserver_adapter.rb
@@ -0,0 +1,20 @@
+module MassInsert
+  module Adapters
+    class SQLServerAdapter < Adapter
+
+      MAX_VALUES_PER_INSERTION = 500
+
+      # This functions calls the necessary functions to create a complete
+      # mysql query to multiple insertion. The methods are in the Abstract
+      # Sql String module. If some method is too specific to this database
+      # adapter you can overwrite it.
+      def execute
+        @values.each_slice(MAX_VALUES_PER_INSERTION).map do |slice|
+          @values = slice
+          begin_string << string_columns << string_values
+        end
+      end
+
+    end
+  end
+end
