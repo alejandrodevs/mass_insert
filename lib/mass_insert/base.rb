14725c1c5608e55173873f9a3751e39543168c01
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-14 10:00:50 -0500

    Adding mass_insert_options method to Base module and specs

--- lib/mass_insert/base.rb
+++ lib/mass_insert/base.rb
@@ -14,20 +14,30 @@ module MassInsert
     #     {:name   => "user name", :email  => "user email"}
     #   ]
     #
-    def mass_insert values, options = {}
-      # prepare default options that come in the class that invokes the
-      # mass_insert function.
-      options[:class_name] = options[:class_name] || self
-      options[:table_name] = options[:table_name] || self.table_name
-
-      # prepare attributes options that were configured by the user and
-      # if the options weren't passed, they would be initialized with the
-      # default values.
-      options[:primary_key]      = options[:primary_key]      || :id
-      options[:primary_key_mode] = options[:primary_key_mode] || :auto
-
-      MassInsert::ProcessControl.new(values, options).execute
+    def mass_insert values, args = {}
+      options = mass_insert_options(args)
+      process = MassInsert::ProcessControl.new(values, options)
+      process.execute
     end
+
+    private
+
+      def mass_insert_options args = {}
+        # prepare default options that come in the class that invokes the
+        # mass_insert function.
+        args[:class_name] = args[:class_name] || self
+        args[:table_name] = args[:table_name] || self.table_name
+
+        # prepare attributes options that were configured by the user and
+        # if the options weren't passed, they would be initialized with the
+        # default values.
+        args[:primary_key]      = args[:primary_key]      || :id
+        args[:primary_key_mode] = args[:primary_key_mode] || :auto
+
+        # Returns the arguments but sanitized and ready to be used. This
+        # args will be passed by params to ProcessControl class.
+        args
+      end
 
   end
 end
--- spec/mass_insert/base_spec.rb
+++ spec/mass_insert/base_spec.rb
@@ -4,6 +4,10 @@ require "./lib/mass_insert"
 describe MassInsert::Base do
   describe "class methods" do
     describe ".mass_insert" do
+      before :each do
+        MassInsert::ProcessControl.any_instance.stub(:execute => true)
+      end
+
       it "should respond to mass_insert class method" do
         User.respond_to?(:mass_insert).should be_true
       end
@@ -22,46 +26,70 @@ describe MassInsert::Base do
       it "should not can called with values" do
         lambda{ User.mass_insert }.should raise_error
       end
+
+      it "should call execute ProcessControl method" do
+        process = MassInsert::ProcessControl.any_instance
+        process.should_receive(:execute).exactly(1).times
+        User.mass_insert([], {})
+      end
+
+      it "should call mass_insert_options method" do
+        User.any_instance.stub(:mass_insert_options)
+        User.should_receive(:mass_insert_options).exactly(1).times
+        User.mass_insert([], {})
+      end
     end
 
-    describe "sanitize options" do
+    describe ".mass_insert_options" do
       describe "class_name" do
-        it "returns class name that call if options doesn't exist" do
-          pending
+        it "returns class name that call if that option doesn't exist" do
+          options = User.send(:mass_insert_options)
+          options[:class_name].should eq(User)
         end
 
         it "returns class_name option if is in the options" do
-          pending
+          args = {:class_name => "OtherClass"}
+          options = User.send(:mass_insert_options, args)
+          options[:class_name].should eq("OtherClass")
         end
       end
 
       describe "table_name" do
         it "returns class table_name that call if options doesn't exist" do
-          pending
+          options = User.send(:mass_insert_options)
+          options[:table_name].should eq(User.table_name)
         end
 
         it "returns table_name option if is in the options" do
-          pending
+          args = {:table_name => "OtherTable"}
+          options = User.send(:mass_insert_options, args)
+          options[:table_name].should eq("OtherTable")
         end
       end
 
       describe "primary_key" do
         it "returns :id if option primary_key doesn't exist" do
-          pending
+          options = User.send(:mass_insert_options)
+          options[:primary_key].should eq(:id)
         end
 
         it "returns primary_key option if is in the options" do
-          pending
+          args = {:primary_key => :user_id}
+          options = User.send(:mass_insert_options, args)
+          options[:primary_key].should eq(:user_id)
         end
       end
 
       describe "primary_key_mode" do
         it "returns :auto if option primary_key_mode doesn't exist" do
-          pending
+          options = User.send(:mass_insert_options)
+          options[:primary_key_mode].should eq(:auto)
         end
 
         it "returns primary_key_mode option if is in the options" do
-          pending
+          args = {:primary_key_mode => :manual}
+          options = User.send(:mass_insert_options, args)
+          options[:primary_key_mode].should eq(:manual)
         end
       end
     end
