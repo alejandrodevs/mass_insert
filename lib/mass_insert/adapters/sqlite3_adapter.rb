6b765ab132caf88ed46c3d270aa0497fa38bf506
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-15 11:28:27 -0500

    Adding comments to SQLite3 and SQLServer adapters

--- lib/mass_insert/adapters/sqlite3_adapter.rb
+++ lib/mass_insert/adapters/sqlite3_adapter.rb
@@ -29,8 +29,7 @@ module MassInsert
 
       def string_single_row_values row
         # Prepare the single row to be included in the sql string.
-        @counter_primary_key = counter_primary_key + 1
-        row.merge!(primary_key => counter_primary_key)
+        row.merge!(primary_key_value)
         row.merge!(timestamp_values) if timestamp?
 
         # Generates the values to this row that will be included according
@@ -38,14 +37,30 @@ module MassInsert
         column_names.map{ |col| string_single_value(row, col) }.join(", ")
       end
 
+			# Returns the current primary key value that is going to be saved
+			# into the database. This primary key value is usually used in
+			# primary_key_value method to generate a primary key hash that will
+			# be merge in row hash.
       def counter_primary_key
         @counter_primary_key ||= class_name.last.try(primary_key).to_i
       end
 
+			# Returns a primary key hash with the next correct value to primary
+			# key column. Here counter_primary_key attributes is incremented by 1
+			# when this method is called. The hash generated usually will be merged
+			# in the row hash.
+			def primary_key_value
+				@counter_primary_key = counter_primary_key + 1
+				{primary_key => counter_primary_key}
+			end
+
       # This functions calls the necessary functions to create a complete
       # sqlite3 query to multiple insertion. The methods are in the Abstract
-      # Query module. If some method is too specific to this database
-      # adapter you can overwrite it.
+      # Query module. If some method is too specific to this database adapter
+			# you can overwrite it. The values that the user gave will be treated
+			# in batches of 500 items because sqlite database allows by default
+			# batches of 500.and each batch will generate a query. This method will
+			# generate an array with batch queries.
       def execute
         @values.each_slice(MAX_VALUES_PER_INSERTION).map do |slice|
           @values = slice
--- lib/mass_insert/adapters/sqlserver_adapter.rb
+++ lib/mass_insert/adapters/sqlserver_adapter.rb
@@ -7,7 +7,10 @@ module MassInsert
       # This functions calls the necessary functions to create a complete
       # sqlserver query to multiple insertion. The methods are in the Abstract
       # Query module. If some method is too specific to this database adapter
-      # you can overwrite it.
+      # you can overwrite it. The values that the user gave will be treated
+			# in batches of 500 items because sqlite database allows by default
+			# batches of 500.and each batch will generate a query. This method will
+			# generate an array with batch queries.
       def execute
         @values.each_slice(MAX_VALUES_PER_INSERTION).map do |slice|
           @values = slice
