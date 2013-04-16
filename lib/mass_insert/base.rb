ee7b375a6c98b0dea6d12d7d6258537469d3b44c
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-16 10:53:59 -0500

    Refactoring mass_insert_options method

--- lib/mass_insert/base.rb
+++ lib/mass_insert/base.rb
@@ -25,14 +25,14 @@ module MassInsert
       def mass_insert_options args = {}
         # prepare default options that come in the class that invokes the
         # mass_insert function.
-        args[:class_name] = args[:class_name] || self
-        args[:table_name] = args[:table_name] || self.table_name
+        args[:class_name] ||= self
+        args[:table_name] ||= self.table_name
 
         # prepare attributes options that were configured by the user and
         # if the options weren't passed, they would be initialized with the
         # default values.
-        args[:primary_key]      = args[:primary_key]      || :id
-        args[:primary_key_mode] = args[:primary_key_mode] || :auto
+        args[:primary_key]      ||= :id
+        args[:primary_key_mode] ||= :auto
 
         # Returns the arguments but sanitized and ready to be used. This
         # args will be passed by params to ProcessControl class.
