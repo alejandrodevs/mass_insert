16d991ba1692356535e2eecbfb301879fcbf3d50
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-17 13:11:15 -0500

    Adding methods to ColumnValue class

--- lib/mass_insert/adapters/column_value.rb
+++ lib/mass_insert/adapters/column_value.rb
@@ -10,6 +10,8 @@ module MassInsert
         @options = options
       end
 
+      # Returns the class that invokes the mass insert process. The class
+      # is in the options hash.
       def class_name
         options[:class_name]
       end
@@ -20,10 +22,14 @@ module MassInsert
         class_name.columns_hash[@column.to_s].type
       end
 
+      # Returns the value to this column in the row hash. The value is
+      # finding by symbol or string key to be most flexible.
       def column_value
-        row[column.to_sym]
+        row[column.to_sym] || row[column.to_s]
       end
 
+      # Returns the string with the database adapter name usually in the
+      # database.yml file in your Rails project.
       def adapter
         ActiveRecord::Base.connection.instance_values["config"][:adapter]
       end
@@ -33,19 +39,19 @@ module MassInsert
       def build
         case column_type
         when :string, :text, :date, :datetime, :time, :timestamp
-          "'#{column_value}'"
+          column_value ? "'#{column_value}'" : "null"
         when :integer
-          column_value.to_i.to_s
+          column_value ? column_value.to_i.to_s : "null"
         when :decimal, :float
-          column_value.to_f.to_s
+          column_value ? column_value.to_f.to_s : "null"
         when :binary
           column_value ? 1 : 0
         when :boolean
           case adapter
           when "mysql2", "postgresql", "sqlserver"
-            column_value ? true : false
+            column_value ? "true" : "false"
           when "sqlite3"
-            column_value ? 0 : 1
+            column_value ? "0" : "1"
           end
         end
       end
