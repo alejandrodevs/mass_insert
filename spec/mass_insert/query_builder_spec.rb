cf69c9808c9a3dc913720ebb137cd4b4ab3d9ce3
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-04-29 13:16:33 -0500

    Refatoring QueryBuilder specs

--- spec/mass_insert/query_builder_spec.rb
+++ spec/mass_insert/query_builder_spec.rb
@@ -2,62 +2,59 @@ require './spec/spec_helper'
 require "./lib/mass_insert"
 
 describe MassInsert::QueryBuilder do
-  before :each do
-    @builder = MassInsert::QueryBuilder.new([], {})
-  end
 
-  subject{ @builder }
+  let!(:subject){ MassInsert::QueryBuilder.new([], {}) }
 
   describe "instance methods" do
     describe "#initialize" do
-      before :each do
-        @builder = MassInsert::QueryBuilder.new("values", "options")
-      end
+
+      let(:builder){ MassInsert::QueryBuilder.new("values", "options")
 
       it "should initialize the values attribute" do
-        @builder.values.should eq("values")
+        builder.values.should eq("values")
       end
 
       it "should initialize the options attribute" do
-        @builder.options.should eq("options")
+        builder.options.should eq("options")
       end
     end
 
     describe "#build" do
       it "should respond to build method" do
-        subject.respond_to?(:build).should be_true
+        expect(subject).to respond_to(:build)
       end
 
       it "should return the query string" do
         subject.stub(:adapter_class).and_return("adapter_class")
         subject.adapter_class.stub(:new).and_return("adapter_instance")
         subject.adapter_class.new.stub(:execute).and_return("query")
-        subject.build.should eq("query")
+        expect(subject.build).to eq("query")
       end
     end
 
     describe "#adapter" do
       it "should respond to adapter method" do
-        subject.respond_to?(:adapter).should be_true
+        expect(subject).to respond_to(:adapter)
       end
 
       it "should return the adapter type" do
         config = {"config" => {:adapter => "sql"}}
-        ActiveRecord::Base.connection.stub(:instance_values).and_return(config)
-        subject.adapter.should eq("sql")
+        connection = ActiveRecord::Base.connection
+        connection.stub(:instance_values).and_return(config)
+        expect(subject.adapter).to eq("sql")
       end
     end
 
     describe "#adapter_class" do
       it "should respond to adapter_class method" do
-        subject.respond_to?(:adapter_class).should be_true
+        expect(subject).to respond_to(:adapter_class)
       end
 
       context "when adapter is mysql2" do
         it "should return a Mysql2Adapter instance" do
           subject.stub(:adapter).and_return("mysql2")
           instance_class = MassInsert::Adapters::Mysql2Adapter
-          subject.adapter_class.should eq(instance_class)
+          expect(subject.adapter_class).to eq(instance_class)
         end
       end
 
@@ -65,7 +62,7 @@ describe MassInsert::QueryBuilder do
         it "should return a PostgreSQLAdapter instance" do
           subject.stub(:adapter).and_return("postgresql")
           instance_class = MassInsert::Adapters::PostgreSQLAdapter
-          subject.adapter_class.should eq(instance_class)
+          expect(subject.adapter_class).to eq(instance_class)
         end
       end
 
@@ -73,7 +70,7 @@ describe MassInsert::QueryBuilder do
         it "should return a SQLite3Adapter instance" do
           subject.stub(:adapter).and_return("sqlite3")
           instance_class = MassInsert::Adapters::SQLite3Adapter
-          subject.adapter_class.should eq(instance_class)
+          expect(subject.adapter_class).to eq(instance_class)
         end
       end
 
@@ -81,7 +78,7 @@ describe MassInsert::QueryBuilder do
         it "should return a SQLServerAdapter instance" do
           subject.stub(:adapter).and_return("sqlserver")
           instance_class = MassInsert::Adapters::SQLServerAdapter
-          subject.adapter_class.should eq(instance_class)
+          expect(subject.adapter_class).to eq(instance_class)
         end
       end
     end
