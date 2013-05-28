a48f2b4a5789a80717b514615b2df730bdfe9e8e
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-05-28 15:30:04 -0500

    Adding specs to test value hashes with string keys

--- spec/active_record_models/model_spec.rb
+++ spec/active_record_models/model_spec.rb
@@ -17,10 +17,63 @@ describe "Model" do
   end
 
   context "when is used without options" do
-    it "should save 5 record into users table" do
-      5.times{ @values << @value_hash }
-      User.mass_insert(@values, @options)
-      User.count.should eq(5)
+    context "when value hashes have string keys" do
+      before :each do
+        value_hash = {
+          "name"    => "some_name",
+          "email"   => "some_email",
+          "age"     => 20,
+          "active"  => true,
+          "checked" => true
+        }
+        5.times{ @values << value_hash }
+        User.mass_insert(@values, @options)
+      end
+
+      it "should save correct values" do
+        User.first.name.should eq("some_name")
+      end
+
+      it "should have saved 5 records" do
+        User.count.should eq(5)
+      end
+    end
+
+    context "when value hashes have symbol keys" do
+      before :each do
+        5.times{ @values << @value_hash }
+        User.mass_insert(@values, @options)
+      end
+
+      it "should save correct values" do
+        User.first.age.should eq(20)
+      end
+
+      it "should have saved 5 records" do
+        User.count.should eq(5)
+      end
+    end
+
+    context "when value hashes have symbol and string keys" do
+      before :each do
+        value_hash = {
+          "name"    => "some_name",
+          :email    => "some_email",
+          "age"     => 20,
+          :active   => true,
+          "checked" => true
+        }
+        5.times{ @values << value_hash }
+        User.mass_insert(@values, @options)
+      end
+
+      it "should save correct values" do
+        User.first.email.should eq("some_email")
+      end
+
+      it "should have saved 5 records" do
+        User.count.should eq(5)
+      end
     end
 
     it "should save if values cointains 1200 records" do
