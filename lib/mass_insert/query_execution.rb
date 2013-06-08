bbf0f6f50fc8aa0ade831b3af64064baf4f6062c
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-06-08 10:38:16 -0500

    Updating some comments in QueryExecution class

--- lib/mass_insert/query_execution.rb
+++ lib/mass_insert/query_execution.rb
@@ -10,15 +10,15 @@ module MassInsert
     # class instances this class. The query can be a string or an array,
     # therefore to be sure that the query_container attribute is an array
     # the param passed to this class is converted to array. The query
-    # container attribute will be iterated in execute method in this class
-    # to execute each query that it contains.
+    # container attribute will be iterated in execute method to execute
+    # each query that it contains.
     def initialize query_container
       @query_container = Array(query_container)
     end
 
-    # Saves queries contained in query_container attributes into database.
-    # Use the helper that ActiveRecord provides. Query container attribute
-    # is iterated to save each query that it contains.
+    # Saves queries contained in query_container attribute into database.
+    # Use the helper that ActiveRecord provides. The query_container
+    # attribute is iterated to save each query that it contains.
     def execute
       @query_container.each do |query|
         ActiveRecord::Base.connection.execute(query)
