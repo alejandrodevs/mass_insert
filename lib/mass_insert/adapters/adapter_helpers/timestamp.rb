76111c385e5f940fd521ff95d43922b2ea3d0ba4
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-06-09 09:38:50 -0500

    Updating comments in Timestamp module

--- lib/mass_insert/adapters/adapter_helpers/timestamp.rb
+++ lib/mass_insert/adapters/adapter_helpers/timestamp.rb
@@ -3,27 +3,25 @@ module MassInsert
     module AdapterHelpers
       module Timestamp
 
-        # Returns true o false if the database table has the
-        # timestamp columns.
+        # Returns true o false if the database table has timestamp columns.
         def timestamp?
           columns.include?(:created_at) && columns.include?(:updated_at)
         end
 
         # Returns timestamp format according to the database adapter. This
-        # function can be overwrite in database adapters classes to put the
-        # correct format to that database.
+        # function can be overwrite in database adapters classes.
         def timestamp_format
           "%Y-%m-%d %H:%M:%S.%6N"
         end
 
-        # Returns the timestamp value with specific format according to the
-        # correct timestamp format to that database.
+        # Returns the timestamp value according to the correct timestamp
+        # format to that database engine.
         def timestamp
           Time.now.strftime(timestamp_format)
         end
 
-        # Returns the timestamp values to be merge into row values that
-        # will be saved in the database.
+        # Returns the timestamp hash to be merge into row values that will
+        # be saved in the database.
         def timestamp_hash
           timestamp_value = timestamp
           {:created_at => timestamp_value, :updated_at => timestamp_value}
