d5ccb34436d1478e57284f06ef056d893f7e0405
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-08-16 09:47:22 -0500

    Removing unnecessary comments to Adapter helpers

--- lib/mass_insert/builder/adapters/helpers/abstract_query.rb
+++ lib/mass_insert/builder/adapters/helpers/abstract_query.rb
@@ -4,49 +4,39 @@ module MassInsert
       module Helpers
         module AbstractQuery
 
-          # Returns a begin string to a basic mysql query insertion. Include
-          # the class table_name and it's included in the string.
+          # Returns a basic beginning of the query.
           def begin_string
             "INSERT INTO #{table_name} "
           end
 
-          # Returns a string  with the column names to the class table name
-          # and divided by commas.
+          # Returns a basic part of the query with the columns definition.
           def string_columns
             "(#{columns.join(", ")}) "
           end
 
-          # Returns the string with all the row values that will be included
-          # in the sql string.
+          # Returns a basic part of the query with all the records values.
           def string_values
             "VALUES (#{string_rows_values});"
           end
 
-          # Gives the correct format to the values string to all rows. This
-          # functions calls a function that will generate a single string row
-          # and at the end all the strings are concatenated.
+          # Returns all the column values to all the records
           def string_rows_values
             values.map{ |row| string_single_row_values(row) }.join("), (")
           end
 
-          # Returns the row column values string to be added in query string
-          # according to the type column and values.
-          # Before that row is prepared with the correct values.
+          # Returns all the column values to a single record.
           def string_single_row_values row
             row.merge!(timestamp_hash) if timestamp?
             columns.map{ |col| string_single_value(row, col) }.join(", ")
           end
 
-          # Returns a single column string value with the correct format and
-          # according to the database configuration, column type and presence.
+          # Returns a single column value. According to the database
+          # configuration, column type and presence.
           def string_single_value row, column
             ColumnValue.new(row, column, class_name).build
           end
 
-          # This functions calls the necessary functions to create a complete
-          # mysql query to multiple insertion. The methods are in the Abstract
-          # Sql String module. If some method is too specific to this database
-          # adapter you can overwrite it.
+          # Calls the necessary methods to create a complete basic query.
           def execute
             "#{begin_string}#{string_columns}#{string_values}"
           end
--- lib/mass_insert/builder/adapters/helpers/column_value.rb
+++ lib/mass_insert/builder/adapters/helpers/column_value.rb
@@ -12,17 +12,13 @@ module MassInsert
             @class_name = class_name
           end
 
-          # Returns a symbol with the column type in the database. The column or
-          # attribute should belongs to the class that invokes the mass insert.
+          # Returns a symbol with the column type according to the database.
           def column_type
             class_name.columns_hash[@column.to_s].type
           end
 
           # Returns the value to this column in the row hash. The value is
-          # finding by symbol or string key to be most flexible. This method
-          # tries to get a value by symbol key with the column name first and
-          # if the symbol key doesn't exist it will try to find it by string
-          # key. Otherwise it will return nil.
+          # finding by symbol or string key to be most flexible.
           def column_value
             row.fetch(column){row[@column.to_s]}
           end
@@ -33,30 +29,26 @@ module MassInsert
             ActiveRecord::Base.connection.instance_values["config"][:adapter]
           end
 
-          # Returns the default value string to be included in query string.
-          # This default value is added to the query if the row hash does not
-          # contains the database column value.
+          # Returns the default column value and it's added to the query if 
+          # the row hash does not contains column value.
           def default_value
             default_db_value ? default_db_value.to_s : "null"
           end
 
-          # Return the database default value using methods that ActiveRecord
-          # provides to see database columns settings.
+          # Returns the default database column value using methods that
+          # ActiveRecord provides.
           def default_db_value
             class_name.columns_hash[@column.to_s].default
           end
 
-          # Returns a single column string value with the correct format and
-          # according to the database configuration, column type and presence.
-          # If the row hash does not include the value to this column return the
-          # default value according to database configuration.
+          # Returns the valid column value to be included in the query.
+          # The value depends of the database engine.
           def build
             column_value.nil? ? default_value : send(:"column_value_#{column_type}")
           end
 
-          # Returns the correct value when the column value is string, text,
-          # date, time, datetime, timestamp. There are alias methods to the
-          # other column types that need a similar query value.
+          # Returns the correct value when column value is string, text,
+          # date, time, datetime, timestamp.
           def column_value_string
             "'#{column_value}'"
           end
@@ -67,40 +59,32 @@ module MassInsert
           alias :column_value_timestamp :column_value_string
           alias :column_value_binary    :column_value_string
 
-          # Returns the correct value to integer column. The column values is
-          # converted to integer to be sure that the value is correct to be
-          # persisted into the database and after it, it's converted to string.
+          # Returns the correct value to integer column.
           def column_value_integer
             column_value.to_i.to_s
           end
 
-          # Returns the correct value to decimal column. There is an alias method
-          # to float type. The column values is converted to decimal to be sure
-          # that the value is correct to be persisted into the database and after
-          # it, it's converted to string.
+          # Returns the correct value to decimal or float column.
           def column_value_decimal
             column_value.to_f.to_s
           end
           alias :column_value_float :column_value_decimal
 
-          # Returns the correct value to boolean column. This column calls the
-          # correct method according to the database adapter to return the correct
-          # value to that database engine.
+          # Returns the correct value to boolean column. This column calls
+          # the correct method according to the database adapter.
           def column_value_boolean
             self.send(:"#{adapter}_column_value_boolean")
           end
 
-          # Returns the column value to boolean column like a string. If the
-          # column value exists returns a true string else false string. There
-          # are alias methods to the database engines that works similarity.
+          # Returns the column value to boolean column to mysql, postgresql
+          # and sqlserver databases.
           def mysql2_column_value_boolean
             column_value ? "true" : "false"
           end
           alias :postgresql_column_value_boolean :mysql2_column_value_boolean
           alias :sqlserver_column_value_boolean  :mysql2_column_value_boolean
 
-          # Returns the column value to boolean column like a string. If the
-          # column value exists returns a "1" else "0".
+          # Returns the column value to boolean column to sqlite database.
           def sqlite3_column_value_boolean
             column_value ? "1" : "0"
           end
