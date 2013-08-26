9aca81a85c45e2305839c356a8a9e1b95f1bff0a
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-08-25 23:21:09 -0500

    Refacting Base module specs

--- lib/mass_insert/base.rb
+++ lib/mass_insert/base.rb
@@ -26,9 +26,13 @@ module MassInsert
     # By default primary key is ignored. If you wish primary key doesn't
     # be ignored you need to pass the primary key option on true.
     #
-    def mass_insert values, args = {}
+    def mass_insert values, options = {}
       extend ClassMethods
-      @mass_insert_process = Process.new(values, mass_insert_options(args))
+
+      options[:class_name]  ||= self
+      options[:primary_key] ||= false
+
+      @mass_insert_process = Process.new(values, options)
       @mass_insert_process.start
     end
 
@@ -39,13 +43,6 @@ module MassInsert
         Result.new(@mass_insert_process)
       end
 
-      private
-
-      def mass_insert_options options = {}
-        options[:class_name]  ||= self
-        options[:primary_key] ||= false
-        options
-      end
     end
   end
 end
--- spec/lib/mass_insert/base_spec.rb
+++ spec/lib/mass_insert/base_spec.rb
@@ -1,88 +1,45 @@
 require 'spec_helper'
 
 describe MassInsert::Base do
-  describe "class methods" do
-    describe ".mass_insert" do
-      before :each do
-        MassInsert::Process.any_instance.stub(:start => true)
-      end
+  let!(:values) { [{name: "name"}] }
+  let!(:options){ {:option_one => "one", :option_two => "two"} }
 
-      it "should respond to mass_insert class method" do
-        expect(Test).to respond_to(:mass_insert)
-      end
+  before :each do
+    MassInsert::Process.any_instance.stub(:start)
+  end
 
-      it "should can receive values and many options" do
-        values  = [{:name => "name"}]
-        options = {:option_one => "one", :option_two => "two"}
-        expect(Test.mass_insert(values, options)).to be_true
-      end
-
-      it "should can receive only values" do
-        values  = [{:name => "name"}]
-        expect(Test.mass_insert(values)).to be_true
-      end
-
-      it "should not can called with values" do
-        expect(lambda{ Test.mass_insert }).to raise_error
-      end
-
-      it "should call execute ProcessControl method" do
-        process = MassInsert::Process.any_instance
-        process.should_receive(:start).exactly(1).times
-        Test.mass_insert([], {})
-      end
-
-      it "should call mass_insert_options method" do
-        Test.any_instance.stub(:mass_insert_options)
-        Test.should_receive(:mass_insert_options).exactly(1).times
-        Test.mass_insert([], {})
-      end
+  describe ".mass_insert" do
+    it "can receive values and options" do
+      expect(lambda{ User.mass_insert(values, options) }).to_not raise_error
     end
 
-    describe ".mass_insert_results" do
-      it "should respond to mass_insert_results class method" do
-        expect(Test).to respond_to(:mass_insert_results)
-      end
-
-      it "should call results method in ProcessControl class" do
-        Test.mass_insert([], {})
-        process = Test.instance_variable_get(:@mass_insert_process)
-        MassInsert::Result.should_receive(:new).with(process)
-        Test.mass_insert_results
-      end
-
-      it "returns a MassInsert::Result instance" do
-        Test.mass_insert([], {})
-        expect(Test.mass_insert_results).to be_an_instance_of(MassInsert::Result)
-      end
+    it "can receive only values" do
+      expect(lambda{ User.mass_insert(values) }).to_not raise_error
     end
 
-    describe ".mass_insert_options" do
-      describe "class_name" do
-        it "returns class name that call if that option doesn't exist" do
-          options = Test.send(:mass_insert_options)
-          expect(options[:class_name]).to eq(Test)
-        end
+    it "should not can called with values" do
+      expect(lambda{ User.mass_insert }).to raise_error
+    end
 
-        it "returns class_name option if is in the options" do
-          args = {:class_name => "OtherClass"}
-          options = Test.send(:mass_insert_options, args)
-          expect(options[:class_name]).to eq("OtherClass")
-        end
-      end
+    it "should call execute ProcessControl method" do
+      MassInsert::Process.any_instance.should_receive(:start)
+      Test.mass_insert(values, options)
+    end
+  end
 
-      describe "primary_key" do
-        it "returns :id if option primary_key doesn't exist" do
-          options = Test.send(:mass_insert_options)
-          expect(options[:primary_key]).to eq(false)
-        end
+  describe ".mass_insert_results" do
+    before :each do
+      User.mass_insert(values, options)
+    end
 
-        it "returns primary_key option if is in the options" do
-          args = {:primary_key => true}
-          options = Test.send(:mass_insert_options, args)
-          expect(options[:primary_key]).to eq(true)
-        end
-      end
+    it "returns a MassInsert::Result instance" do
+      expect(User.mass_insert_results).to be_an_instance_of(MassInsert::Result)
+    end
+
+    it "calls new MassInsert::Result method with the process" do
+      process = User.instance_variable_get(:@mass_insert_process)
+      MassInsert::Result.should_receive(:new).with(process)
+      User.mass_insert_results
     end
   end
 end
