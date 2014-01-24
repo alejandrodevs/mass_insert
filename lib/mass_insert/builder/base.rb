11e68db7ac203720981bc389b0072bcc6bdb025a
JoseLuis Torres <joseluis@powhow.com>
2014-01-24 11:56:34 -0600

    simple fix to avoid using case syntax

--- lib/mass_insert/builder/base.rb
+++ lib/mass_insert/builder/base.rb
@@ -1,7 +1,12 @@
 module MassInsert
   module Builder
     class Base
-
+      ADAPTERS = { 
+         'mysql2' => Adapters::Mysql2Adapter,
+         'postgresql' => Adapters::PostgreSQLAdapter,
+         'sqlite3' => Adapters::SQLite3Adapter,
+         'sqlserver' => Adapters::SQLServerAdapter
+      }
       # This function gets the correct adapter class and returns the
       # sql string ready to be executed.
       def build values, options
@@ -11,16 +16,7 @@ module MassInsert
       # Returns the class of the correct database adapter according to the
       # database engine that is used in Rails project.
       def adapter_class
-        case Utilities.adapter
-        when "mysql2"
-          Adapters::Mysql2Adapter
-        when "postgresql"
-          Adapters::PostgreSQLAdapter
-        when "sqlite3"
-          Adapters::SQLite3Adapter
-        when "sqlserver"
-          Adapters::SQLServerAdapter
-        end
+        ADAPTERS[Utilities.adapter]
       end
 
     end
